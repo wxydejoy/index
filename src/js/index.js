cde50fc80fd856cf4a2c7f77a73e279b1547247b
wxydejoy <1798923421@qq.com>
2021-12-16 22:56:23 +0800

    index

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+function load(){
+
+    model.src='https://ts.undf.top/3d/bysj.html' 
+    document.getElementById("model").style.display = "block"
+    document.getElementById("button").style.display = "none"
+}
+
